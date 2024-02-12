ab3ab93fefbbae29b6c9f8ac3056a429600b3c9d
prashant9984 <prashantsahani129@gmail.com>
2024-02-11 20:13:11 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "4",
+    spaceBetween: 15,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+
+
